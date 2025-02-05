22452c6833df8e4eff61cfa41f2dc25c31279bff
Aleksander Laasmägi <aleksander.laasmagi@gmail.com>
2025-02-05 15:19:44 +0200

    Made splash screen stay if android device offline

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import InitialSelection from './app/screens/InitialSelection';
 import LoginView from './app/screens/LoginView';
@@ -7,10 +7,29 @@ import CreateAccountView from './app/screens/CreateAccountView'
 import SettingsView from './app/screens/SettingsView'
 import {NavigationContainer}  from '@react-navigation/native';
 import  {createStackNavigator}  from '@react-navigation/stack';
+import { SplashScreen } from 'expo-router';
+import { StyleSheet, Image, ActivityIndicator, View, Text, TouchableOpacity } from 'react-native';
 
 const Stack = createStackNavigator();
+SplashScreen.preventAutoHideAsync();
+
+const API_URL = "https://google.com";
 
 export default function App() {
+  useEffect(() => {
+    checkAPIConnection();
+  }, []);
+
+  const checkAPIConnection = async () => {
+
+  const response = await fetch(API_URL);
+    if (response.ok) {
+      setTimeout(() => SplashScreen.hideAsync(), 3000);
+    } else {
+      throw new Error("Server error");
+    }
+  };
+
   return (
     <>
     <StatusBar style="light"/>
