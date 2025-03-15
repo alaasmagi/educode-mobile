155f72a91655ffe259007657b8ba2930fed782c3
Aleksander Laasmägi <allaas@taltech.ee>
2025-03-15 02:36:27 +0200

    Solved more errors

--- app.config.js
+++ app.config.js
@@ -1,21 +1,49 @@
 export default {
-    expo: {
-      name: "EduCode",
-      slug: "educode-mobile",
-      splash: {
-        image: "./app/assets/logos/splash-logo.png",
-        resizeMode: "contain",
+  expo: {
+    name: "EduCode",
+    slug: "educode-mobile",
+    version: "1.0.0",
+    orientation: "portrait",
+    userInterfaceStyle: "dark",
+    newArchEnabled: true,
+    splash: {
+      image: "./app/assets/logos/splash-logo.png",
+      resizeMode: "contain",
+      backgroundColor: "#2B2B2B"
+    },
+    ios: {
+      supportsTablet: true,
+      icon: "./app/assets/logos/app-icon.png"
+    },
+    android: {
+      package: "com.educode.mobile",
+      adaptiveIcon: {
+        foregroundImage: "./app/assets/logos/app-icon.png",
         backgroundColor: "#2B2B2B"
       },
-    android: {
-        package: "com.educode.mobile"
+      permissions: ["android.permission.CAMERA"]
     },
-      plugins: ["expo-router"],
+    web: {
+      favicon: "./app/assets/favicon.png"
+    },
+    plugins: [
+      "expo-router",
+      [
+        "expo-camera",
+        {
+          cameraPermission: "Allow $(PRODUCT_NAME) to access your camera",
+          recordAudioAndroid: false
+        }
+      ]
+    ],
     extra: {
-        eas: {
-            projectId: "6a2ef7e1-936b-4005-a953-e2d4393bf9dd"
-        },
-        EXPO_PUBLIC_API_URL: process.env.EXPO_PUBLIC_API_URL || "https://educode.share.zrok.io/"
+      router: {
+        origin: false
       },
-    },
-  };
+      EXPO_PUBLIC_API_URL: process.env.EXPO_PUBLIC_API_URL || "https://educode.share.zrok.io/",
+      eas: {
+        projectId: "6a2ef7e1-936b-4005-a953-e2d4393bf9dd"
+      }
+    }
+  }
+};
